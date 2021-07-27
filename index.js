a7475b20a17b127e53c87041c3965f36129cd67b
Gustavo Mendes <g.mendes93@hotmail.com>
2021-07-26 21:58:19 -0300

    feature:modificacoes finais

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const http = require('http')
 
 http.createServer((req, res) => {
-        res.end('Servidor funcionando!')
+    // res.writeHead(200, {
+    //     'Content-Type': 'text/html;charset=utf-8'
+    // })
+    // res.end('<h1>Servidor funcionando!</h1>')
+    res.writeHead(200, {
+        'Content-Type': 'application ld/json;charset=utf-8'
+    })
+    res.end('Servidor Funcionando!'
+    )
 }).listen(3000, 'localhost', () => console.log('O servidor está rondando...'))
